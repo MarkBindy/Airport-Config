1a5e40c2f9fb5a393d4b63562e89a5d7e012f5f2
MarkBindy <39180063+MarkBindy@users.noreply.github.com>
2026-09-22 12:57:43 +0800

    Update RewriteAUTO.js

--- RewriteAUTO.js
+++ RewriteAUTO.js
@@ -466,7 +466,7 @@ function main(config) {
     });
 
     existingRegionalFallbacks.push(fallbackName);
-    allRegionalGroupNames.push(fallbackName);  // 按顺序排列：故障转移 -> 手动
+    allRegionalGroupNames.push(fallbackName, autoName);  // 按顺序排列：故障转移 -> 手动
   });
 
   // ============================================================
