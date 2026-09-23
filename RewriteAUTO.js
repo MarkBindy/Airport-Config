ff7d3a6a659208d050caf42fcb1ff42d2928af54
MarkBindy <39180063+MarkBindy@users.noreply.github.com>
2026-09-23 20:15:09 +0800

    Update RewriteAUTO.js

--- RewriteAUTO.js
+++ RewriteAUTO.js
@@ -432,7 +432,10 @@ function main(config) {
   const existingRegionalAutos = [];
 
   regionGroups.forEach(region => {
-    const matched = currentProxyNames.filter(name => region.filter.test(name));
+    const matched = currentProxyNames.filter(
+      name => region.filter.test(name)
+    );
+
     if (matched.length === 0) return;   // 满足条件数，才会生成该地区相应节点组
 
     const fallbackName = region.name + "-故障转移";
